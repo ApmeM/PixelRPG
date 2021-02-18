dc9ab2431813343407d4fe507231bc8d23f7f6aa
artem.votincev <artem.votincev@gmail.com>
2021-02-18 17:48:59 +0400

    Add visible fog of war

---
+++ PixelRPG.Base/Content/assets/fog.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="fog" tilewidth="16" tileheight="16" tilecount="2" columns="2">
+ <image source="fog.png" trans="000000" width="32" height="16"/>
+</tileset>
